70be21650d7af96b25cc52b6db68cdf3317e2d64
ntw222 <kubiakb1@gmail.com>
2017-12-07 22:58:19 +0100

    Updated app component

--- client/pizza-store/src/app/app.module.ts
+++ client/pizza-store/src/app/app.module.ts
@@ -6,21 +6,29 @@ import {UserComponent} from "./user/user.component";
 import {UserService} from "./user-service/user.service";
 import {HttpClientModule} from "@angular/common/http";
 import {RouterModule} from "@angular/router";
+import { OrderComponent } from './order/order.component';
+import { ProductComponent } from './product/product.component';
+import {OrderService} from "./order-service/order.service";
+import {ProductService} from "./product-service/product.service";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    UserComponent
+    UserComponent,
+    OrderComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
     RouterModule.forRoot([
-      { path:'users', component: UserComponent }
+      { path:'users', component: UserComponent },
+      { path:'orders', component: OrderComponent },
+      { path:'products', component: ProductComponent }
     ])
   ],
-  providers: [UserService],
+  providers: [UserService, OrderService, ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
