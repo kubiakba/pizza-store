d856261905b21ac6ba211e4de297dcb900fe25ce
ntw222 <kubiakb1@gmail.com>
2017-12-07 22:45:18 +0100

    Added product component

---
+++ client/pizza-store/src/app/product-service/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable()
+export class ProductService {
+
+  url ="/api/products/";
+  pizzaUrl ="pizzas";
+  kebabUrl ="kebabs";
+  pizzaToppingUrl ="pizzaToppings";
+
+  constructor(private http: HttpClient) { }
+
+  public getPizzas(): Observable<any[]> {
+    return this.http.get<any[]>(this.url + this.pizzaUrl);
+  }
+
+  public getKebabs(): Observable<any[]> {
+    return this.http.get<any[]>(this.url + this.kebabUrl);
+  }
+
+  public getPizzaToppings(): Observable<any[]> {
+    return this.http.get<any[]>(this.url + this.pizzaToppingUrl);
+  }
+
+}
