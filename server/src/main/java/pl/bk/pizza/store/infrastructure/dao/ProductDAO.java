6accec51306f712c43f3dc58a16453c6756f8099
ntw222 <kubiakb1@gmail.com>
2017-12-07 17:00:59 +0100

    Added extra endopints for specific products

--- server/src/main/java/pl/bk/pizza/store/infrastructure/dao/ProductDAO.java
+++ server/src/main/java/pl/bk/pizza/store/infrastructure/dao/ProductDAO.java
@@ -1,12 +1,20 @@
 package pl.bk.pizza.store.infrastructure.dao;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
+import pl.bk.pizza.store.domain.product.Kebab;
+import pl.bk.pizza.store.domain.product.Pizza;
+import pl.bk.pizza.store.domain.product.PizzaTopping;
 import pl.bk.pizza.store.domain.product.Product;
 import pl.bk.pizza.store.domain.product.ProductRepository;
 
 import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Repository
 public class ProductDAO implements ProductRepository {
@@ -32,6 +40,25 @@ public class ProductDAO implements ProductRepository {
     @Override
     public List<Product> getAllProducts() {
         return mongoTemplate.findAll(Product.class, COLLECTION_NAME);
+    }
+
+    @Override
+    public List<Product> getAllPizzas() {
+        return mongoTemplate.find(query(where("productInfo._class").is(Pizza.class.getCanonicalName())),
+            Product.class, COLLECTION_NAME);
+    }
+
+    @Override
+    public List<Product> getAllKebabs() {
+        Query query = new Query().addCriteria(Criteria.where("productInfo._class").is(Kebab.class.getCanonicalName()));
+        return mongoTemplate.find(query, Product.class, COLLECTION_NAME);
+    }
+
+    @Override
+    public List<Product> getAllPizzaToppings() {
+        Query query = new Query().addCriteria(Criteria.where("productInfo._class").is(
+            PizzaTopping.class.getCanonicalName()));
+        return mongoTemplate.find(query, Product.class, COLLECTION_NAME);
     }
 
 
