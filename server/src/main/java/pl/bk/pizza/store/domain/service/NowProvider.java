65bc48b58663cb902b462b01775c020d8bbe2010
ntw222 <kubiakb1@gmail.com>
2017-12-15 19:06:54 +0100

    Util class for providing actual timeDate

---
+++ server/src/main/java/pl/bk/pizza/store/domain/service/NowProvider.java
@@ -0,0 +1,13 @@
+package pl.bk.pizza.store.domain.service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class NowProvider {
+
+    private static final String DEFAULT_ZONE_NAME = "Europe/Warsaw";
+
+    public static LocalDateTime now(){
+        return LocalDateTime.now(ZoneId.of(DEFAULT_ZONE_NAME));
+    }
+}
