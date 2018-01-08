35c7f7a943e456d8eae2fca023eda2efcdb27df3
ntw222 <kubiakb1@gmail.com>
2018-01-08 16:56:46 +0100

    Setting user role in User Factory

--- server/src/main/java/pl/bk/pizza/store/domain/user/UserFactory.java
+++ server/src/main/java/pl/bk/pizza/store/domain/user/UserFactory.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserFactory {
 
+    private final static String USER_ROLE = "USER";
+
     public User createUser(String email, String name, String surname, String password, Address address, Telephone telephone, String role) {
+        if(role == null){
+            role = USER_ROLE;
+        }
         return new User(email, name, surname, new BCryptPasswordEncoder().encode(password), address, telephone,
             role);
     }
