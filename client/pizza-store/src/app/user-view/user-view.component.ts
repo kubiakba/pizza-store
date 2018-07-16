4b1b0314e0d18cef3e912158d0b6d66f539b8b9d
kubiakb <kubiakb11@gmail.com>
2018-07-16 18:09:51 +0200

    Refactored user-view package

--- client/pizza-store/src/app/user-view/user-view.component.ts
+++ client/pizza-store/src/app/user-view/user-view.component.ts
@@ -1,22 +1,58 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {UserService} from "../user/user.service";
 import {User} from "../user/user";
 
 @Component({
   selector: 'app-user-view',
-  templateUrl: './user-view.component.html',
-  styleUrls: ['./user-view.component.css']
+  template: `
+    <div style="background-color: #93B874;">
+      <form class="form form-horizontal">
+        <div class="form-group">
+          <div class="col-md-3">
+            <input (keydown.enter)="search($event)" class="form-control" placeholder="Provide email address">
+          </div>
+        </div>
+      </form>
+      <div *ngIf="user">
+        <table class="table">
+          <thead>
+          <tr>
+            <th>Email</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>City</th>
+            <th>Street</th>
+            <th>Street number</th>
+            <th>Post code</th>
+            <th>Telephone</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>{{user.email}}</td>
+            <td>{{user.name}}</td>
+            <td>{{user.surname}}</td>
+            <td>{{user.address.city}}</td>
+            <td>{{user.address.street}}</td>
+            <td>{{user.address.streetNumber}}</td>
+            <td>{{user.address.postCode}}</td>
+            <td>{{user.telephone.number}}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  `
 })
-export class UserViewComponent implements OnInit {
+export class UserViewComponent {
 
   user: User;
-  sth = "Something something bad";
 
   constructor(private userService: UserService) {
   }
 
-  ngOnInit() {
-    this.userService.getUser("1232@gmail.com")
-      .subscribe((result: User) => this.user = result);
+  search(event: any) {
+    this.userService.getUser(event.target.value)
+      .subscribe((result: any) => this.user = new User(result));
   }
 }
