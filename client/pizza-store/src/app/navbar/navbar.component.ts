06f832dc060ea2c65e4d82a08f9d8c18e0659341
kubiakb <kubiakb11@gmail.com>
2018-07-16 17:28:12 +0200

    Moved navbar html to component

--- client/pizza-store/src/app/navbar/navbar.component.ts
+++ client/pizza-store/src/app/navbar/navbar.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  template: `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" routerLink="/users-view">user</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" routerLink="/products-view">products</a>
+      </li>
+    </ul>
+  </div>
+</nav>`
 })
 export class NavbarComponent implements OnInit {
 
