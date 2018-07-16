0f15fe2158e99040e7a84891a6a8a952f78f2e35
kubiakb <kubiakb11@gmail.com>
2018-07-16 10:53:05 +0200

    Added navbar component

--- client/pizza-store/src/app/app.module.ts
+++ client/pizza-store/src/app/app.module.ts
@@ -1,19 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
-import { AppComponent } from './app.component';
-import { UserViewComponent } from './user-view/user-view.component';
+import {AppComponent} from './app.component';
+import {UserViewComponent} from './user-view/user-view.component';
+import {RouterModule} from "@angular/router";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserViewComponent
+    UserViewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([{
+      path: 'user',
+      component: UserViewComponent
+    },{
+      path: 'products',
+      component: UserViewComponent
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
