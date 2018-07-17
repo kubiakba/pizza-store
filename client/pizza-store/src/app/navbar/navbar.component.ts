a9b4b9663c7dca9f6ceaf25873d186d9bd8bbe05
kubiakb <kubiakb11@gmail.com>
2018-07-17 14:05:19 +0200

    Changed navbar html

--- client/pizza-store/src/app/navbar/navbar.component.ts
+++ client/pizza-store/src/app/navbar/navbar.component.ts
@@ -2,18 +2,23 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  template: `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" routerLink="/view-users">user</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" routerLink="/view-products">products</a>
-      </li>
-    </ul>
-  </div>
-</nav>`
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #d43f3a">
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-1 order-md-0">
+      <ul class="navbar-nav ml-auto text-center">
+        <li class="nav-item active">
+          <a style="font-size:30px" class="nav-link" routerLink="view-products">Menu</a>
+        </li>
+      </ul>
+    </div>
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-2 order-md-2">
+      <ul class="navbar-nav mr-auto text-center">
+        <li class="nav-item active">
+          <a style="font-size: 30px" class="nav-link" routerLink="view-contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>`
 })
 export class NavbarComponent{
 }
