13d8390e0387dc99bb24409cc4a12a5b9ba2c9c4
kubiakb <kubiakb11@gmail.com>
2018-07-19 11:15:35 +0200

    Add host argument to example-data script

--- example-data.sh
+++ example-data.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+host=$1
+
 #create products
-product_id=$(curl --silent -d '{"type":"Kebab", "price":10.0, "description":"Small","kebabName":"Fet Kebab"}' -H "Content-Type: application/json" -X POST http://localhost:8081/products | awk -F ":||," '{print $4}' | awk '{print substr($0, 2, length($0) - 2)}')
-curl -d '{"type":"PizzaTopping", "price":6.0, "toppingName":"salami"}' -H "Content-Type: application/json" -X POST http://localhost:8081/products
-curl -d '{"type":"Pizza", "price":8.0, "size":"SMALL","dough":"THIN"}' -H "Content-Type: application/json" -X POST http://localhost:8081/products
+product_id=$(curl --silent -d '{"type":"Kebab", "price":10.0, "description":"Small","kebabName":"Fet Kebab"}' -H "Content-Type: application/json" -X POST $host/products | awk -F ":||," '{print $4}' | awk '{print substr($0, 2, length($0) - 2)}')
+curl -d '{"type":"PizzaTopping", "price":6.0, "toppingName":"salami"}' -H "Content-Type: application/json" -X POST $host/products
+curl -d '{"type":"Pizza", "price":8.0, "size":"SMALL","dough":"THIN"}' -H "Content-Type: application/json" -X POST $host/products
 
 #create users
-curl -d '{"email":"1232@gmail.com","password":"pass","name":"Adam","surname":"Kościelniak","address":{"city":"Poznań","street":"kośćielna","streetNumber":"22","postCode":"31-283"},"telephone":{"number":"234"},"role":"USER"}' -H "Content-Type: application/json" -X POST http://localhost:8081/users
-curl -d '{"email":"1@gmail.com","password":"pass","name":"Witold","surname":"Król","address":{"city":"Warszawa","street":"Niepodległości","streetNumber":"34","postCode":"43-344"},"telephone":{"number":"234"},"role":"USER"}' -H "Content-Type: application/json" -X POST http://localhost:8081/users
+curl -d '{"email":"1232@gmail.com","password":"pass","name":"Adam","surname":"Kościelniak","address":{"city":"Poznań","street":"kośćielna","streetNumber":"22","postCode":"31-283"},"telephone":{"number":"234"},"role":"USER"}' -H "Content-Type: application/json" -X POST $host/users
+curl -d '{"email":"1@gmail.com","password":"pass","name":"Witold","surname":"Król","address":{"city":"Warszawa","street":"Niepodległości","streetNumber":"34","postCode":"43-344"},"telephone":{"number":"234"},"role":"USER"}' -H "Content-Type: application/json" -X POST $host/users
 
 #create orders
-order_id=$(curl --silent -d '{"email":"1@gmail.com"}' -H "Content-Type: application/json" -X POST http://localhost:8081/orders | awk -F ":||," '{print $2}' | awk '{print substr($0, 2, length($0) - 2)}')
+order_id=$(curl --silent -d '{"email":"1@gmail.com"}' -H "Content-Type: application/json" -X POST $host/orders | awk -F ":||," '{print $2}' | awk '{print substr($0, 2, length($0) - 2)}')
 
 #add product to order
-curl -H "Content-Type: application/json" -X PUT http://localhost:8081/orders/$order_id/$product_id
+curl -H "Content-Type: application/json" -X PUT $host/orders/$order_id/$product_id
