056aca380dad2d117170845670d1f1097132f3df
kubiakb <kubiakb11@gmail.com>
2018-07-24 22:13:48 +0200

    configure tests so webClient has csrf token and appropriate role

--- server/src/test/groovy/pl/bk/pizza/store/helpers/CommonSpecification.groovy
+++ server/src/test/groovy/pl/bk/pizza/store/helpers/CommonSpecification.groovy
@@ -4,13 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.reactive.server.WebTestClient
 import spock.lang.Specification
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf
 
 @AutoConfigureWebTestClient(timeout = "300000")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@WithMockUser(username = "admin", roles = "USER")
 class CommonSpecification extends Specification implements OrderHelper, ProductHelper, UserHelper
 {
     @Autowired
@@ -30,6 +33,6 @@ class CommonSpecification extends Specification implements OrderHelper, ProductH
     @Override
     WebTestClient getClient()
     {
-        return client
+        return client.mutateWith(csrf())
     }
 }
