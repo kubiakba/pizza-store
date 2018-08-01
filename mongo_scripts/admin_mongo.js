7b21261b1b0b79bea6f340b299d9ca8d0e98e931
kubiakb <kubiakb11@gmail.com>
2018-08-01 21:18:58 +0200

    Add status="ACTIVE" field to admin user

--- mongo_scripts/admin_mongo.js
+++ mongo_scripts/admin_mongo.js
@@ -1,3 +1,11 @@
 /*password = admin*/
 
-db.user.insert( { "_id": "admin@admin.pl","name":"B","surname":"K","password":"{bcrypt}$2a$10$GW1WQ.VAg.hhov/GnBl3Z.qb7ddkHro2jEJtaO1ozcHbQw6rO6S8G", "role": "ADMIN","_class":"pl.bk.pizza.store.domain.customer.user.User" } )
+db.user.insert({
+    "_id": "admin@admin.pl",
+    "name": "B",
+    "surname": "K",
+    "password": "{bcrypt}$2a$10$GW1WQ.VAg.hhov/GnBl3Z.qb7ddkHro2jEJtaO1ozcHbQw6rO6S8G",
+    "role": "ADMIN",
+    "status": "ACTIVE",
+    "_class": "pl.bk.pizza.store.domain.customer.user.User"
+});
