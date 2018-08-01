c6d8521c72ab4109eed727a590d5691e05f81dca
kubiakb <kubiakb11@gmail.com>
2018-08-01 15:25:37 +0200

    Binding between user object and inputs

--- client/pizza-store/src/app/add-user-view/add-user-view.component.ts
+++ client/pizza-store/src/app/add-user-view/add-user-view.component.ts
@@ -9,43 +9,43 @@ import {NewOrder} from "../order/newOrder";
       <div class="form-group row">
         <label for="name" class="col-sm-1 col-form-label">Name</label>
         <div class="col-xm-4">
-          <input type="text" class="form-control" id="Name">
+          <input type="text" [(ngModel)]="user.name" name="name" class="form-control" id="Name">
         </div>
       </div>
       <div class="form-group row">
         <label for="surname" class="col-sm-1 col-form-label">Surname</label>
         <div class="col-xm-4">
-          <input type="text" class="form-control" id="surname">
+          <input type="text" [(ngModel)]="user.surname" name="surname"  class="form-control" id="surname">
         </div>
       </div>
       <div class="form-group row">
         <label for="telephone" class="col-sm-1 col-form-label">Telephone</label>
         <div class="col-xm-4">
-          <input type="text" class="form-control" id="telephone">
+          <input type="text" [(ngModel)]="user.telephone" name="telephone" class="form-control" id="telephone">
         </div>
       </div>
       <div class="form-group row">
         <label for="city" class="col-sm-1 col-form-label">City</label>
         <div class="col-xm-4">
-          <input type="text" class="form-control" id="city">
+          <input type="text" [(ngModel)]="user.city" name="city" class="form-control" id="city">
         </div>
       </div>
       <div class="form-group row">
         <label for="street" class="col-sm-1 col-form-label">Street</label>
         <div class="col-xm-4">
-          <input type="text" class="form-control" id="street">
+          <input type="text" [(ngModel)]="user.street" name="street" class="form-control" id="street">
         </div>
       </div>
       <div class="form-group row">
         <label for="streetNumber" class="col-sm-1 col-form-label">Number</label>
         <div class="col-xm-4">
-          <input type="text" class="form-control" id="streetNumber">
+          <input type="text" [(ngModel)]="user.streetNumber" name="streetNumber" class="form-control" id="streetNumber">
         </div>
       </div>
       <div class="form-group row">
         <label for="zipCode" class="col-sm-1 col-form-label">Zip Code</label>
         <div class="col-xm-4">
-          <input type="text" class="form-control" id="zipCode">
+          <input type="text" [(ngModel)]="user.zipCode" name="zipCode" class="form-control" id="zipCode">
         </div>
       </div>
     </form>
@@ -56,6 +56,7 @@ export class AddUserViewComponent {
 
   orderId: String;
   @Input() order: NewOrder;
+  user: any = {};
 
   constructor(private orderService: OrderService) {
   }
