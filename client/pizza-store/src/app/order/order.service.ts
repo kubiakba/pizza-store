597808a8b2f65a2a003ddf4c1c2d5904f01b8b06
kubiakb <kubiakb11@gmail.com>
2018-08-02 12:15:55 +0200

    new method to start order with email

--- client/pizza-store/src/app/order/order.service.ts
+++ client/pizza-store/src/app/order/order.service.ts
@@ -23,6 +23,10 @@ export class OrderService {
     return this.http.post<any>(this.order_url, this.empty_body, this.httpOptions);
   }
 
+  public startOrderWithEmail(email:String): Observable<any> {
+    return this.http.post<any>(this.order_url, JSON.stringify({email:email}), this.httpOptions);
+  }
+
   public addProductToOrder(orderId: String, productId: String): Observable<any> {
     return this.http.put<any>(this.order_url + `/${orderId}/${productId}`, this.empty_body, this.httpOptions);
   }
