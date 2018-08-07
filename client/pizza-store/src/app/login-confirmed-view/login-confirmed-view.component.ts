e9f96c08895c580846550e87cfd8d4e26331fbaa
kubiakb <kubiakb11@gmail.com>
2018-08-07 17:07:07 +0200

    Login confirmed view added

---
+++ client/pizza-store/src/app/login-confirmed-view/login-confirmed-view.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-login-confirmed-view',
+  template: `<p>You are logged !</p>`
+})
+export class LoginConfirmedViewComponent {
+}
