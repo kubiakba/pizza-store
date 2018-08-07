e164375b00aec27c4759331d33cc3116fa73814d
kubiakb <kubiakb11@gmail.com>
2018-08-07 17:09:03 +0200

    Error handling in login-view-component

--- client/pizza-store/src/app/login-view/login-view.component.ts
+++ client/pizza-store/src/app/login-view/login-view.component.ts
@@ -7,35 +7,49 @@ import {JwtConsts} from "../authentication/jwt-consts";
   selector: 'app-login-view',
   template: `
     <div style="background-color: #c3e6cb">
-      <div class="register">
-        Login
+      <div *ngIf="!userHasBeenLoggedSuccessfully">
+        <div class="register">
+          Login
+        </div>
+        <br>
+        <form>
+          <div class="form-group">
+            <label for="emailId">Email address</label>
+            <input type="email" [(ngModel)]="email" name="email" class="form-control" id="emailId" placeholder="Enter email">
+          </div>
+          <div class="form-group">
+            <label for="passId">Password</label>
+            <input type="password" [(ngModel)]="password" name="password" class="form-control" id="passId" placeholder="Password">
+          </div>
+          <div *ngIf="errorMessage.size > 0">
+            <div *ngFor="let message of errorMessage">
+              {{message}}
+            </div>
+          </div>
+          <button (click)="login()" type="submit" class="btn btn-primary">Submit</button>
+        </form>
       </div>
-      <br>
-      <form>
-        <div class="form-group">
-          <label for="emailId">Email address</label>
-          <input type="email" [(ngModel)]="email" name="email" class="form-control" id="emailId" placeholder="Enter email">
-        </div>
-        <div class="form-group">
-          <label for="passId">Password</label>
-          <input type="password" [(ngModel)]="password" name="password" class="form-control" id="passId" placeholder="Password">
-        </div>
-        <button (click)="login()" type="submit" class="btn btn-primary">Submit</button>
-      </form>
+      <div *ngIf="userHasBeenLoggedSuccessfully">
+        <app-login-confirmed-view></app-login-confirmed-view>
+      </div>
     </div>`
 })
 export class LoginViewComponent {
 
   email: String = '';
   password: String = '';
+  errorMessage: Set<String> = new Set<String>();
+  userHasBeenLoggedSuccessfully = false;
 
   constructor(private userService: UserService) {
   }
 
   login() {
     this.userService.login(new JwtAuthenticationRequest(this.email, this.password)).subscribe(token => {
-      localStorage.setItem(JwtConsts.localStorageTokenKey, token.token);
-    });
+        localStorage.setItem(JwtConsts.localStorageTokenKey, token.token);
+        this.userHasBeenLoggedSuccessfully = true;
+      }
+      , error => this.errorMessage.add(error.error.errorCode));
   }
 
 }
