5dae28aaa7d16f21ce6701136bb4e9ed5090e405
kubiakb <kubiakb11@gmail.com>
2018-08-08 17:36:15 +0200

    Updated scripts

--- mongo_scripts/admin_mongo.js
+++ mongo_scripts/admin_mongo.js
@@ -2,8 +2,6 @@
 
 db.user.insert({
     "_id": "admin@admin.pl",
-    "name": "B",
-    "surname": "K",
     "password": "{bcrypt}$2a$10$GW1WQ.VAg.hhov/GnBl3Z.qb7ddkHro2jEJtaO1ozcHbQw6rO6S8G",
     "role": "ADMIN",
     "status": "ACTIVE",
