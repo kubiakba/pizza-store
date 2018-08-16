6ed8b1d3c626bd8c6017ebdca75017b9fed65f03
kubiakb <kubiakb11@gmail.com>
2018-08-16 15:20:34 +0200

    Remove dto serializers

--- server/src/main/java/pl/bk/pizza/store/domain/broker/OrderListener.java
+++ server/src/main/java/pl/bk/pizza/store/domain/broker/OrderListener.java
@@ -1,13 +1,11 @@
 package pl.bk.pizza.store.domain.broker;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import lombok.SneakyThrows;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import pl.bk.pizza.store.application.dto.order.OrderDTO;
-import pl.bk.pizza.store.domain.serializers.order.OrderDeserializer;
 
 @Component
 public class OrderListener
@@ -17,9 +15,6 @@ public class OrderListener
     public void listen(ConsumerRecord<String, String> record)
     {
         final ObjectMapper mapper = new ObjectMapper();
-        final SimpleModule simpleModule = new SimpleModule();
-        simpleModule.addDeserializer(OrderDTO.class, new OrderDeserializer());
-        mapper.registerModule(simpleModule);
         OrderDTO order = mapper.readValue(record.value(), OrderDTO.class);
     }
 }
