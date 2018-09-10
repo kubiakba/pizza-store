ded6d6d447c9c772e77ad03e79a5cef1b5622a71
kubiakb <kubiakb1@gmail.com>
2018-09-10 13:08:03 +0200

    Scan packages from all modules

--- server/src/main/java/pl/bk/pizza/store/Initializer.java
+++ server/src/main/java/pl/bk/pizza/store/Initializer.java
@@ -2,8 +2,10 @@ package pl.bk.pizza.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"pl.bk.search", "pl.bk.common", "pl.bk.pizza"})
 public class Initializer
 {
     public static void main(String[] args)
